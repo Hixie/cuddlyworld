339b7bef82d4cc928b8f2317c9fdf0758fff8ae1
Ian Hickson <ian@hixie.ch>
2015-02-28 06:42:23 +0000

    Improve Threshold output

--- threshold.pas
+++ threshold.pas
@@ -23,24 +23,17 @@ type
       property BackSideDescription: UTF8String read FBackSideDescription write FBackSideDescription;
    end;
 
-   // Should seriously consider factoring out the Landmark stuff in
-   // TLocation just so we don't have to have the overhead here.
-   // Just remember to take care of the ground and sky...
    TThresholdLocation = class(TSurfaceSlavedLocation) // @RegisterStorableClass
-    protected
-      FThreshold: TThresholdThing;
-      procedure ProxiedFindMatchingThings(Perspective: TAvatar; Options: TFindMatchingThingsOptions; PositionFilter: TThingPositionFilter; PropertyFilter: TThingFeatures; List: TThingList); override;
-      function ProxiedFindThingTraverser(Thing: TThing; Perspective: TAvatar; FromOutside: Boolean): Boolean; override;
     public
       constructor Create(Landmark: TThresholdThing; Surface: TThing);
-      constructor Read(Stream: TReadStream); override;
-      procedure Write(Stream: TWriteStream); override;
       function GetLookTowardsDirectionDefault(Perspective: TAvatar; Direction: TCardinalDirection): UTF8String; override;
-      // GetDescriptionDirectional?
+      function GetDescriptionHere(Perspective: TAvatar; Mode: TGetPresenceStatementMode; Context: TAtom = nil): UTF8String; override;
       function GetDescriptionRemoteDetailed(Perspective: TAvatar; Direction: TCardinalDirection): UTF8String; override;
       procedure AddExplicitlyReferencedThingsDirectional(Tokens: TTokens; Start: Cardinal; Perspective: TAvatar; Distance: Cardinal; Direction: TCardinalDirection; Reporter: TThingReporter); override;
       // XXX make this give the landmark thing's description when the place is examined
       function GetEntrance(Traveller: TThing; Direction: TCardinalDirection; Perspective: TAvatar; var PositionOverride: TThingPosition; var DisambiguationOpening: TThing; var Message: TMessage; NotificationList: TAtomList): TAtom; override;
+      procedure ProxiedFindMatchingThings(Perspective: TAvatar; Options: TFindMatchingThingsOptions; PositionFilter: TThingPositionFilter; PropertyFilter: TThingFeatures; List: TThingList); override;
+      function ProxiedFindThingTraverser(Thing: TThing; Perspective: TAvatar; FromOutside: Boolean): Boolean; override;
    end;
 
 function ConnectThreshold(FrontLocation, BackLocation: TLocation; Threshold: TThresholdThing; Surface: TThing; Flags: TLocation.TLandmarkOptions = [loAutoDescribe]): TThresholdLocation;
@@ -59,6 +52,7 @@ begin
    BackLocation.AddLandmark(Threshold.FrontSideFacesDirection, Result, Flags);
    Result.AddLandmark(cdReverse[Threshold.FrontSideFacesDirection], BackLocation, [loAutoDescribe, loPermissibleNavigationTarget]);
 end;
+
 
 constructor TThresholdThing.Create(Name: UTF8String; Pattern: UTF8String; Description: UTF8String; FrontFacesDirection: TCardinalDirection);
 begin
@@ -105,66 +99,54 @@ end;
 
 constructor TThresholdLocation.Create(Landmark: TThresholdThing; Surface: TThing);
 begin
-   inherited Create(Landmark, tpAt, Surface);
-   FThreshold := Landmark;
-end;
-
-constructor TThresholdLocation.Read(Stream: TReadStream);
-begin
-   inherited;
-   Stream.ReadReference(@Pointer(FThreshold));
-end;
-
-procedure TThresholdLocation.Write(Stream: TWriteStream);
-begin
-   inherited;
-   Stream.WriteReference(FThreshold);
+   inherited Create(Landmark, tpAtImplicit, Surface);
 end;
 
 function TThresholdLocation.GetLookTowardsDirectionDefault(Perspective: TAvatar; Direction: TCardinalDirection): UTF8String;
 begin
-   Result := FThreshold.GetPresenceStatement(Perspective, psThereIsAThingThere);
+   Result := FMaster.GetPresenceStatement(Perspective, psThereIsAThingThere);
+end;
+
+function TThresholdLocation.GetDescriptionHere(Perspective: TAvatar; Mode: TGetPresenceStatementMode; Context: TAtom = nil): UTF8String;
+begin
+   Assert(Assigned(FMaster));
+   if (Context <> FMaster) then
+      Result := FMaster.GetDescriptionSelf(Perspective) + WithNewlineIfMultiline(inherited)
+   else
+      Result := inherited;
 end;
 
 function TThresholdLocation.GetDescriptionRemoteDetailed(Perspective: TAvatar; Direction: TCardinalDirection): UTF8String;
 begin
-   Result := FThreshold.GetDescriptionRemoteDetailed(Perspective, Direction);
+   Result := FMaster.GetDescriptionRemoteDetailed(Perspective, Direction);
 end;
 
 procedure TThresholdLocation.AddExplicitlyReferencedThingsDirectional(Tokens: TTokens; Start: Cardinal; Perspective: TAvatar; Distance: Cardinal; Direction: TCardinalDirection; Reporter: TThingReporter);
 begin
    if (Distance > 0) then
-      FThreshold.AddExplicitlyReferencedThings(Tokens, Start, Perspective, True, Reporter);
+      FMaster.AddExplicitlyReferencedThings(Tokens, Start, Perspective, True, Reporter);
    inherited;
 end;
 
 function TThresholdLocation.ProxiedFindThingTraverser(Thing: TThing; Perspective: TAvatar; FromOutside: Boolean): Boolean;
 begin
-   Result := FThreshold.FindThingTraverser(Thing, Perspective, FromOutside);
+   Result := FMaster.FindThingTraverser(Thing, Perspective, FromOutside);
 end;
 
 procedure TThresholdLocation.ProxiedFindMatchingThings(Perspective: TAvatar; Options: TFindMatchingThingsOptions; PositionFilter: TThingPositionFilter; PropertyFilter: TThingFeatures; List: TThingList);
 begin
-   FThreshold.FindMatchingThings(Perspective, Options, PositionFilter, PropertyFilter, List);
+   FMaster.FindMatchingThings(Perspective, Options, PositionFilter, PropertyFilter, List);
 end;
 
 function TThresholdLocation.GetEntrance(Traveller: TThing; Direction: TCardinalDirection; Perspective: TAvatar; var PositionOverride: TThingPosition; var DisambiguationOpening: TThing; var Message: TMessage; NotificationList: TAtomList): TAtom;
 begin
-   DisambiguationOpening := FThreshold;
-   NotificationList.AppendItem(FThreshold);
+   DisambiguationOpening := FMaster;
+   NotificationList.AppendItem(FMaster);
    Result := GetAtomForDirectionalNavigation(Direction);
    if (Assigned(Result)) then
       Result := Result.GetEntrance(Traveller, Direction, Perspective, PositionOverride, DisambiguationOpening, Message, NotificationList)
    else
       Result := inherited;
-end;
-
-
-function ConnectThreshold(SourceLocation: TLocation; Destination: TLocation; Surface: TThing; Threshold: TThresholdThing): TThresholdLocation;
-begin
-   Result := TThresholdLocation.Create(Threshold, Surface);
-   ConnectLocations(Destination, Threshold.FrontSideFacesDirection, Result);
-   ConnectLocations(Result, Threshold.FrontSideFacesDirection, SourceLocation);
 end;
 
 initialization
